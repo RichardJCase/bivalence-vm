8b4c884cacee87d3da4d12ca2497c5272ced7f47
RichardJCase <RichardJacobCase@gmail.com>
2019-06-02 20:32:51 -0500

    created base lanauage grammer type

--- compiler/AST.hs
+++ compiler/AST.hs
@@ -1,7 +1,21 @@
 module AST where
 
-data ID = ID String deriving (Show, Eq, Ord)
-data Type = Type String deriving Show
+data ID = ID String
+data Type = Type String
+data Operator = Operator String
+data Literal = Literal String
 
-data IDList = IDElem ID IDList | EmptyString deriving Show
-data ParamList = Param Type ID ParamList | EmptyString deriving Show
+data ExprList = ExprList Expr ExprList | ExprListEmptyString
+data Expr = Expr Signature Operator PropList Defn
+data Signature = Signature Type ID ParamList
+
+data IDList = IDList ID IDList | IDListEmptyString
+data ParamList = ParamList Type ID ParamList | ParamListEmptyString
+
+data Defn = DefnApplication Application | DefnImplication Implication
+data OutVars = OutVars Operator IDList
+data Application = Application ID IDList OutVars
+
+data Implication = Implication ID Operator IDList
+data Prop = Prop ID Operator Defn
+data PropList = PropList Prop PropList | PropListEmptyString
